fb715329a22d84a90ff4ee8fd4619ef9dac08cd9
sunil kumar sahu <mr.sahu39@gmail.com>
2024-05-04 20:18:44 +0530

    Section14 operator overloading as non member function or global function

---
+++ CPP_workspaces/Section14/04_Mystring_Operator_Overloading_Global_Function/Mystring.cpp
@@ -0,0 +1,75 @@
+#include <cstring>
+#include <iostream>
+#include <Mystring.h>
+
+Mystring::Mystring()
+    :str{nullptr}{
+        str = new char[1];
+        *str = '\0';
+}
+
+//overloaded Constructor
+Mystring::Mystring(const char *source)
+    :str(nullptr){
+        if(source == nullptr)
+        {
+            str = new char[1];
+            *str = '\0';
+        }
+        else
+        {
+            str = new char[std::strlen(source)+1];
+            std::strcpy(str,source);
+        }
+}
+
+Mystring::Mystring(const Mystring &source)
+    :str(nullptr){
+        str = new char[std::strlen(source.str)+1];
+        std::strcpy(str,source.str);
+}
+//Copy Assignment
+Mystring &Mystring::operator =(const Mystring &rhs)
+{
+    std::cout << "Copy Assignment Operator" << std::endl;
+    if(this == &rhs)
+    {
+        return *this;
+    }
+    delete[] this->str;
+    str = new char[std::strlen(rhs.str) + 1];
+    std::strcpy(this->str,rhs.str);
+    return *this;
+}
+
+Mystring::Mystring(Mystring &&source)
+{
+    this->str = source.str;
+    source.str = nullptr;
+    std::cout << "Move Constructor is used" << std::endl;
+}
+
+Mystring & Mystring::operator=(Mystring &&rhs)
+{
+    std::cout<<"Move Assignment Operator"<<std::endl;
+    if(this == &rhs)
+        return *this;
+    delete[] this->str;
+    this->str = rhs.str;
+    rhs.str = nullptr;
+    return *this;
+}
+Mystring::~Mystring()
+{
+    std::cout<<"Destructor is called for:"<<this<<std::endl;
+    delete[]str;
+}
+
+int Mystring::get_length() const{return std::strlen(str);}
+
+const char *Mystring::get_str() const{return str;}
+
+void Mystring::display() const
+{
+    std::cout <<str<<":"<<get_length()<<std::endl;
+}
