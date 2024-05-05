40acace1e4f0ed537e4f646c2f875af04d796881
sunil kumar sahu <mr.sahu39@gmail.com>
2024-05-05 16:44:51 +0530

    Section14 insertion extraction operator overloading code added

---
+++ CPP_workspaces/Section14/05_Mystring_operator_overloading_insertion_extraction/Mystring.h
@@ -0,0 +1,22 @@
+#ifndef _MYSTRING_H
+#define _MYSTRING_H
+
+class Mystring
+{
+private:
+    char *str;//Pointer to char[] which holds c-style string
+public:
+    Mystring();                       //Default Constructor
+    Mystring(const char *s);          //Overloaded Constructor
+    Mystring(const Mystring &source); //Copy Constructor
+    Mystring(Mystring &&source);      //Move Constructor
+    ~Mystring();                      //Destructor
+    
+    Mystring &operator=(const Mystring &rhs); //Copy Assisgnment
+    Mystring &operator=(Mystring &&rhs);//Move Assignment
+    void display() const;
+    int get_length() const;
+    const char *get_str() const;
+    
+};
+#endif
