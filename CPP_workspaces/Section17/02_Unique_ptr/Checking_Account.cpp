260287e04b60062d46f69f268c219f65adce3e08
sunil kumar sahu <mr.sahu39@gmail.com>
2024-07-18 05:00:11 +0530

    Unique pointer Account Example

---
+++ CPP_workspaces/Section17/02_Unique_ptr/Checking_Account.cpp
@@ -0,0 +1,23 @@
+#include "Checking_Account.h"
+
+Checking_Account::Checking_Account(std::string name, double balance)
+    : Account {name, balance} {
+}
+
+bool Checking_Account::withdraw(double amount) {
+    amount += per_check_fee;
+    return Account::withdraw(amount);
+}
+
+bool Checking_Account::deposit(double amount){
+    return Account::deposit(amount);
+}
+
+void Checking_Account::print(std::ostream &os) const{
+    os << "[Checking_Account: " << name << ": " << balance  << "]";
+}
+/*std::ostream &operator<<(std::ostream &os, const Checking_Account &account) {
+    os << "[Checking_Account: " << account.name << ": " << account.balance  << "]";
+    return os;
+}*/
+
