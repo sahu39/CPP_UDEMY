9240f1c8ff7a54e42a0bb70b3795c98a92cb05bc
sahu39 <45738762+sahu39@users.noreply.github.com>
2024-10-12 18:48:32 +0530

    Section10 CPP_C_String_02.cpp file modified

--- CPP_workspaces/Section10/CPP_String_eg/CPP_C_String_02.cpp
+++ CPP_workspaces/Section10/CPP_String_eg/CPP_C_String_02.cpp
@@ -1,3 +1,11 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
 #include <iostream>
 #include <cstring>
 #include <string>
@@ -23,17 +31,17 @@ int main()
     
     //nInitialization
     cout << "\nInitialization" <<"\n------------------------" << endl;
-    cout << "s1 initialized to:" << s1 << endl;
-    cout << "s2 initialized to:" << s2 << endl;
-    cout << "s3 initialized to:" << s3 << endl;
-    cout << "s4 initialized to:" << s4 << endl;
-    cout << "s5 initialized to:" << s5 << endl;
-    cout << "s6 initialized to:" << s6 << endl;
-    cout << "s7 initialized to:" << s7 << endl;
-    cout << "my_name initialized to:" << my_name << endl;
-    cout << "str_my_name initialized to:" << str_my_name << endl;
-    cout << "str_my_name2 initialized to:" << str_my_name2 << endl;
-    cout << "str_my_name3 initialized to:" << str_my_name3 << endl;
+    cout << "s1 initialized to:" << s1 << endl;//Apple
+    cout << "s2 initialized to:" << s2 << endl;//Banana
+    cout << "s3 initialized to:" << s3 << endl;//Kiwi
+    cout << "s4 initialized to:" << s4 << endl;//Apple
+    cout << "s5 initialized to:" << s5 << endl;//Apple
+    cout << "s6 initialized to:" << s6 << endl;//App
+    cout << "s7 initialized to:" << s7 << endl;//XXXXXXXXXX
+    cout << "my_name initialized to:" << my_name << endl;//Sunil
+    cout << "str_my_name initialized to:" << str_my_name << endl;//Sunil
+    cout << "str_my_name2 initialized to:" << str_my_name2 << endl;//Sunil  Kumar  Sahu
+    cout << "str_my_name3 initialized to:" << str_my_name3 << endl;//Sunil  Kumar  Sahu
     
     //length & size
     cout << "\nLength and Size" << "\n---------------------" << endl;
@@ -44,9 +52,9 @@ int main()
     cout << "s5 length:" << s5.length() << " s5 size:" << s5.size() << endl;
     cout << "s6 length:" << s6.length() << " s6 size:" << s6.size() << endl;
     cout << "s7 length:" << s7.length() << " s7 size:" << s7.size() << endl;
-    cout << "my_name length:" << strlen(my_name) << " my_name size:" << sizeof(my_name)<< endl;
-    cout << "str_my_name length:" << str_my_name.length() << " str_my_name size:" << str_my_name.size() << endl;
-    cout << "str_my_name2 length:" << str_my_name2.length() << " str_my_name2 size:" << str_my_name2.size() << endl;
-    cout << "str_my_name3 length:" << str_my_name3.length() << " str_my_name3 size:" << str_my_name3.size() << endl;
+    cout << "my_name length:" << strlen(my_name) << " my_name size:" << sizeof(my_name)<< endl;//6,30
+    cout << "str_my_name length:" << str_my_name.length() << " str_my_name size:" << str_my_name.size() << endl;//6,6
+    cout << "str_my_name2 length:" << str_my_name2.length() << " str_my_name2 size:" << str_my_name2.size() << endl;//30,30
+    cout << "str_my_name3 length:" << str_my_name3.length() << " str_my_name3 size:" << str_my_name3.size() << endl;//30,30
     return 0;
 }
